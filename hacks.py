c77583a69473d98de3661e7d49f61ee03766cbbc
negashev.al <aleksandr.negashev@domru.ru>
2017-12-18 11:46:24 +0500

    add last_modify_file function

--- hacks.py
+++ hacks.py
@@ -11,3 +11,16 @@ def mkdir_with_chmod(path):
 def get_dirs(path):
     return [os.path.join(path, o) for o in os.listdir(path)
             if os.path.isdir(os.path.join(path, o))]
+
+
+def last_modify_file(dirname="."):
+    newer_file, newer_time = None, None
+    for dirpath, dirs, files in os.walk(dirname):
+        for filename in files:
+            file_path = os.path.join(dirpath, filename)
+            file_time = os.stat(file_path).st_mtime
+            if newer_time is None:
+                newer_file, newer_time = file_path, file_time
+            if file_time > newer_time:
+                newer_file, newer_time = file_path, file_time
+    return newer_file, newer_time
--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,10 @@
-import glob
 import os
 import time
 import uuid
 import shutil
 from subprocess import call
 
-from hacks import mkdir_with_chmod, get_dirs
+from hacks import mkdir_with_chmod, get_dirs, last_modify_file
 
 
 class Config(object):
@@ -60,8 +59,8 @@ def create_blanks():
     source_path = mkdir_with_chmod('source')
     this_time = time.time()
     # if source data not ready
-    filedate = os.path.getmtime(source_path)
-    if this_time - filedate < 20.0:
+    newer_file, newer_time = last_modify_file(source_path)
+    if this_time - newer_time < 20.0:
         return
     # create new path for blank
     new_dir = str(uuid.uuid4())
@@ -86,8 +85,8 @@ def check_blanks():
     source_path = mkdir_with_chmod('source')
     this_time = time.time()
     # if source data not ready
-    filedate = os.path.getmtime(source_path)
-    if this_time - filedate < 20.0:
+    newer_file, newer_time = last_modify_file(source_path)
+    if this_time - newer_time < 20.0:
         return
     # get ready blanks
     blanks_dirs = get_dirs(blanks_path)
@@ -103,16 +102,15 @@ def check_blanks():
                     # if uuid update (new source data)
                     if source_uuid != blank_uuid:
                         # if this cold dir
-                        filedate = os.path.getmtime(blank_dir)
-                        if this_time - filedate > 60.0:
+                        newer_file, newer_time = last_modify_file(blank_dir)
+                        if this_time - newer_time > 60.0:
                             remove_uuid = blank_dir
             except Exception as e:
                 print(e)
                 try:
                     # if this broken blank remove them
-                    filedate = os.path.getmtime(blank_dir)
-                    # if this cold dir
-                    if this_time - filedate > 60.0:
+                    newer_file, newer_time = last_modify_file(blank_dir)
+                    if this_time - newer_time > 60.0:
                         remove_uuid = blank_dir
                 except Exception as e:
                     print(e)
@@ -135,9 +133,8 @@ def clean_mounted():
         if uuid is not None:
             break
         # if this cold dir very long
-        newest = max(glob.glob(os.path.join(mounted_dir, '*')), key=os.path.getmtime)
-        filedate = os.path.getmtime(newest)
-        if this_time - filedate > 60.0 * 10:
+        newer_file, newer_time = last_modify_file(mounted_dir)
+        if this_time - newer_time > 60.0 * 10:
             uuid = os.path.basename(mounted_dir)
     if uuid is not None:
         # remove service!
@@ -168,8 +165,7 @@ def clean_tmp():
     this_time = time.time()
     for tmp_dir in tmp_dirs:
         # if this cold dir very long
-        newest = max(glob.glob(os.path.join(tmp_dir, '*')), key=os.path.getmtime)
-        filedate = os.path.getmtime(newest)
-        if this_time - filedate > 60.0 * 60:
+        newer_file, newer_time = last_modify_file(tmp_dir)
+        if this_time - newer_time > 60.0 * 60:
             print(f'remove freeze tmp {uuid}')
             shutil.rmtree(tmp_dir)
