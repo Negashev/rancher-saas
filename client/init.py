5fbfb82d2676c1c4262791616064181415d69f85
negashev.al <aleksandr.negashev@domru.ru>
2018-01-15 11:16:22 +0500

    Fix infinite delivery with freeze get uuid

--- client/init.py
+++ client/init.py
@@ -7,6 +7,8 @@ from socketIO_client import SocketIO, BaseNamespace
 
 from get_service_address import get_service_address
 
+delivery = True
+
 
 def check_open_port(address):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,15 +27,18 @@ def check_open_port(address):
 class ChatNamespace(BaseNamespace):
 
     def on_health_check(self, uuid):
+        global delivery
         if uuid:
             exit(0)
         else:
+            delivery = False
             print("You old service not found")
             chat_namespace.emit('get uuid')
 
     def on_set_uuid(self, uuid):
+        global delivery
+        delivery = False
         print('Delivery directory')
-
         self.emit('delivery', {'uuid': uuid})
 
     def on_delivery(self, data):
@@ -59,19 +64,23 @@ class ChatNamespace(BaseNamespace):
             exit(0)
 
 
-socketIO = SocketIO(os.getenv('SAAS_DELIVERY_URL', '10.100.31.41'), int(os.getenv('SAAS_DELIVERY_PORT', 8080)))
-chat_namespace = socketIO.define(ChatNamespace, '/saas')
-
 # check local save file and ping service if exist
 with open('/tmp/local.file', 'w') as local:
     PROXY_ADDR = os.getenv("PROXY_ADDR", "0.0.0.0:80")
     if PROXY_ADDR.startswith("tcp://"):
         PROXY_ADDR = PROXY_ADDR[len("tcp://"):]
     local.write(PROXY_ADDR)
-if os.path.isfile('/tmp/proxy.file'):
-    print("Found proxy config, try connect to service")
-    chat_namespace.emit('health check', get_service_address())
-else:
-    print("Start delivery")
-    chat_namespace.emit('get uuid')
-socketIO.wait()
+
+while delivery:
+    print("Connecting to delivery service")
+    socketIO = SocketIO(os.getenv('SAAS_DELIVERY_URL', '10.100.31.41'), int(os.getenv('SAAS_DELIVERY_PORT', 8080)))
+    chat_namespace = socketIO.define(ChatNamespace, '/saas')
+    print("Success connect")
+    if os.path.isfile('/tmp/proxy.file'):
+        print("Found proxy config, try connect to service")
+        chat_namespace.emit('health check', get_service_address())
+    else:
+        print("Start delivery")
+        chat_namespace.emit('get uuid')
+    socketIO.wait(120)
+    print('Did not wait for service, retry')
