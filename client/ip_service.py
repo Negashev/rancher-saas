b423d1bbf4c3cc5973b784d8a374f7ba72910f17
negashev.al <aleksandr.negashev@domru.ru>
2018-03-20 14:18:29 +0500

    Add get uuid for service

--- client/ip_service.py
+++ client/ip_service.py
@@ -1,12 +1,17 @@
 import os
 from japronto import Application
 from get_service_address import get_service_address
+from service_uuid import get_service_uuid
 
 
 def gsa(request):
     return request.Response(text=get_service_address())
 
+def guuid(request):
+    return request.Response(text=get_service_uuid())
+
 
 app = Application()
 app.router.add_route('/', gsa)
+app.router.add_route('/uuid', guuid)
 app.run(host='0.0.0.0', port=int(os.getenv('IP_SERVICE_PORT', 8080)))
